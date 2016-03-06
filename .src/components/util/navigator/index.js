d49cc31c3375937f61897e6932b014e16e13f87d
Dafrok <o.o@mug.dog>
2016-03-07 01:12:29 +0800

    update icon

--- .src/components/util/navigator/index.js
+++ .src/components/util/navigator/index.js
@@ -6,6 +6,11 @@ import NavStore from '../../../stores/nav.js'
 import {Actions} from '../../../stores/nav.js'
 import {Link} from 'react-router'
 import connect from '../../../lib/connect.js'
+import Delete from 'material-ui/lib/svg-icons/action/delete';
+import Insert from 'material-ui/lib/svg-icons/editor/insert-drive-file';
+import Folder from 'material-ui/lib/svg-icons/file/folder';
+import Favor from 'material-ui/lib/svg-icons/action/grade';
+import Auth from 'material-ui/lib/svg-icons/action/supervisor-account';
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -37,11 +42,11 @@ export default class Header extends React.Component {
         width={200}
         open={this.state.open}
         onRequestChange={open => Actions.toggleNav(open)}>
-        <MenuItem linkButton={true} containerElement={<Link to="/editor" />}>New Note</MenuItem>
-        <MenuItem linkButton={true} containerElement={<Link to="/article-list" />}>My Notes</MenuItem>
-        <MenuItem linkButton={true} containerElement={<Link to="/recycle" />}>Recycle</MenuItem>
-        <MenuItem linkButton={true} containerElement={<Link to="/about" />}>About</MenuItem>
-        <MenuItem linkButton={true} containerElement={<Link to="/login" onClick={this.logout} />}>{connect.getAuth() ? 'Logout' : 'Login'}</MenuItem>
+        <MenuItem leftIcon={<Insert />} linkButton={true} containerElement={<Link to="/editor" />}>New Note</MenuItem>
+        <MenuItem leftIcon={<Folder />} linkButton={true} containerElement={<Link to="/article-list" />}>My Notes</MenuItem>
+        <MenuItem leftIcon={<Delete />} linkButton={true} containerElement={<Link to="/recycle" />}>Recycle</MenuItem>
+        <MenuItem leftIcon={<Favor />} linkButton={true} containerElement={<Link to="/about" />}>About</MenuItem>
+        <MenuItem leftIcon={<Auth />} linkButton={true} containerElement={<Link to="/login" onClick={this.logout} />}>{connect.getAuth() ? 'Logout' : 'Login'}</MenuItem>
       </LeftNav>
     );
   }

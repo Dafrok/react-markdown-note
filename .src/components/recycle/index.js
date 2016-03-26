9c5aef43ce66c3cd9e69ee21c3e27c49bbd2143a
Dafrok <o.o@mug.dog>
2016-03-26 08:13:46 +0800

    recycle

---
+++ .src/components/recycle/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Divider from 'material-ui/lib/divider';
+import Avatar from 'material-ui/lib/avatar';
+import Colors from 'material-ui/lib/styles/colors';
+import IconButton from 'material-ui/lib/icon-button';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import {Link} from 'react-router'
+import Header from '../util/header.js'
+import StatusBar from '../util/status-bar'
+import Navigator from '../util/navigator'
+import connect from '../../lib/connect.js'
+
+export default class Recycle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: []
+    }
+  }
+  delete = id => {
+    connect.child('recycle').child(connect.getAuth().auth.uid).remove()
+    this.getList()
+  }
+  getList = () => {
+    connect.child('recycle').child(connect.getAuth().auth.uid).orderByChild('time').once("value", snapshot => {
+      let list = []
+      snapshot.forEach(snap => {
+        let value = snap.val()
+        value.id = snap.key()
+        list.unshift(value)
+      })
+      this.setState({articles: list})
+    }, errorObject => {
+      console.log("The read failed: " + errorObject.code);
+    })
+  }
+  componentDidMount = () => {
+    this.getList()
+  }
+  render () {
+    return (
+      <div>
+      <Navigator />
+      <StatusBar>
+        <Header />
+      </StatusBar>
+      <List>
+        {
+          this.state.articles.map((item, index) => {
+            return (<ListItem
+              key={item.id}
+              primaryText={item.summary}
+              secondaryText={item.time}
+            />)
+          })
+        }
+      </List>
+      </div>
+    )
+  }
+}
+
+export default ArticleList;
--- .src/index.js
+++ .src/index.js
@@ -9,6 +9,7 @@ import About from './components/about'
 import Preview from './components/preview'
 import Editor from './components/editor'
 import Login from './components/login'
+import Recycle from './components/recycle'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import connect from './lib/connect.js'
 
@@ -28,6 +29,7 @@ ReactDOM.render(
       <Route path="editor/:noteId" component={Editor} onEnter={needLogin}/>
       <Route path="article-list" component={ArticleList} onEnter={needLogin}/>
       <Route path="preview/:noteId" component={Preview} onEnter={needLogin}/>
+      <Route path="recycle" component={Recycle} onEnter={needLogin}/>
       <Route path="about" component={About} onEnter={needLogin}/>
       <Route path="login" component={Login}/>
     </Route>

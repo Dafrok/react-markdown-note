78360fe31774202d7750daaaefae2a0a35b53990
Dafrok <o.o@mug.dog>
2016-03-28 22:07:32 +0800

    remove useless space

--- .src/components/article-list/index.js
+++ .src/components/article-list/index.js
@@ -77,13 +77,13 @@ export default class ArticleList extends React.Component {
               primaryText={item.title}
               secondaryText={((date, fmt) => {
                   var o = {
-                      "M+": date.getMonth() + 1,
-                      "d+": date.getDate(),
-                      "h+": date.getHours(),
-                      "m+": date.getMinutes(),
-                      "s+": date.getSeconds(),
-                      "q+": Math.floor((date.getMonth() + 3) / 3),
-                      "S": date.getMilliseconds()
+                    "M+": date.getMonth() + 1,
+                    "d+": date.getDate(),
+                    "h+": date.getHours(),
+                    "m+": date.getMinutes(),
+                    "s+": date.getSeconds(),
+                    "q+": Math.floor((date.getMonth() + 3) / 3),
+                    "S": date.getMilliseconds()
                   };
                   if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
                   for (var k in o)
